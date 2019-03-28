5619ec7477ba7f88842e8de55ecdeb2feaeb9f83
Liam Searle <liamsearle7@gmail.com>
2019-03-28 15:55:36 +0200

    did some work on question b

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -1,8 +1,63 @@
 ﻿module ICPC
 open System
 
+type letters = {//this is totally wrong
+  |'a'
+  |'b'
+  |'c'
+  |'d'
+  |'e'
+  |'f'
+  |'g'
+  |'h'
+  |'i'
+  |'j'
+  |'k'
+  |'l'
+  |'m'
+  |'n'
+  |'o'
+  |'p'
+  |'q'
+  |'r'
+  |'s'
+  |'t'
+  |'u'
+  |'v'
+  |'w'
+  |'x'
+  |'y'
+  |'z'
+}
+
+let checkLetter input = //takes in a char and checks if it is a valid char
+
+  match input with
+  |'a' |'b' |'c' |'d' |'e' |'f' |'g' |'h' |'i' |'j' |'k' |'l' |'m' |'n' |'o' |'p' |'q' |'r' |'s' |'t' |'u' |'v' |'w' |'x' |'y' |'z'|','|' '|'.' -> true
+  |_ -> false
+
+  let checkInbetweenWords input = 
+    match input with
+    | [_;'.';letters;_]| [_;',';letters;_] -> Some ()
+    | [_;' ';' ';_] -> None
+
 let commaSprinkler input =
-    failwith "Not implemented"
+  let inputList = Seq.toList input //convert the input string to a list
+  //---------------Checks to make sure the input string is valid-----------------
+  let checkLength input = //kick out the function if the length is wrong
+    match inputList.Length>1 with
+    | false -> None
+    | _-> Some ()
+
+  let isLegalInput input = 
+    match (List.exists checkLetter inputList) with
+    |false -> None
+    |_ -> Some ()
+  
+
+  
+  //---------------End Checks to make sure the input string is valid-------------
+    //failwith "Not implemented"
 
 let rivers input =
     failwith "Not implemented"
