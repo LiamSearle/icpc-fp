2cc589e5f66458b12024a61f1db3fea73e183454
Liam Searle <liamsearle7@gmail.com>
2019-04-16 22:26:58 +0200

    Rules is working properly its actually fixed now...wow

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -18,6 +18,46 @@ let listOfCharLists (input: string) = //words = [ ['O';'n';'e';','];  ['t';'w';'
   let words = List.map (fun c -> Seq.toList c) wordList//Seq.toList wordList
   words
 
+let rec checkCommaSuffix input = 
+  match input with
+  |[] -> true
+  |head::tail -> match head with
+                 |',' -> match tail.Head with 
+                               |' ' -> (checkCommaSuffix tail)
+                               |_ -> false
+                 |_-> checkCommaSuffix tail
+
+let rec checkSpaceSuffix input = 
+  match input with
+  |[] -> true
+  |head::tail -> match head with
+                 |' ' -> match System.Char.IsLetter(tail.Head) with 
+                               |true -> (checkCommaSuffix tail)
+                               |_ -> false
+                 |_-> checkCommaSuffix tail
+
+let rec checkStopSuffix input = 
+  match input with
+  |[] -> true
+  |head::tail -> match tail with 
+                 |[] -> true
+                 |_ ->
+                     match head with
+                     |'.' -> match tail.Head with 
+                                   |' ' -> (checkStopSuffix tail)
+                                   |_ -> false
+                     |_-> checkStopSuffix tail
+
+(*
+let rec checkStopPrefix input = 
+  match List.rev input with
+  |[] -> true
+  |head::tail -> match head with
+                 |'.' -> match tail.Head with 
+                               |' ' -> false
+                               |_ -> (checkStopPrefix tail)
+                 |_-> checkStopPrefix tail
+*)
 
 let listOfChars (input: string) = 
   let wordList = Seq.toList input
@@ -41,9 +81,9 @@ let shaker input =
                    |',' -> [head] @ shake tail
                    |_ -> shake tail
   shake words
-  let wordyBoy = shake words
+  //let wordyBoy = shake words
 
-  Console.WriteLine(sprintf "%A" wordyBoy)
+  //Console.WriteLine(sprintf "%A" wordyBoy)
   //Console.WriteLine(sprintf "asdasdasd")
   //let listyBoi = shake
   //let noComma = Char. (fun c -> Char.is
@@ -78,21 +118,27 @@ let rules input =
   |"" -> None //cant be empty
   |_ ->
     let words = listOfCharLists input
+    let stringList = listOfStrings input
     let firstElem = words.Head
     let charList = listOfChars input
-    let cleanList = List.filter (fun c -> c <> ' ' && c <> '.' && c <> ',') charList
+    let cleanList = (List.filter (fun c -> c <> ' ' && c <> '.' && c <> ',') charList)
     let lastChar = charList.[charList.Length-1]
-    
-    Console.WriteLine(sprintf "%A" charList)
-    match ( cleanList |> List.exists (fun c -> c = System.Char.ToUpper c)) with //cant be any upper case letters
-    |false ->    
-      match firstElem.Length >= 2 with //must be a word, most words are more than 2 chars right?
-      |true -> 
-        match lastChar with
-        |'.' -> Some ("Hello") //sentence must end with a fullstop
-        |_ -> None
-      |_ -> None
+    match input.Contains("  ") || input.Contains(" .") with
     |true -> None
+    |false ->
+      match (checkCommaSuffix charList && checkSpaceSuffix charList && checkStopSuffix charList) with
+      |false -> None
+      |true ->
+        Console.WriteLine(sprintf "%A" stringList)
+        match ( cleanList |> List.exists (fun c -> c = System.Char.ToUpper c)) with //cant be any upper case letters
+        |false ->    
+          match firstElem.Length >= 2 with //must be a word, most words are more than 2 chars right?
+          |true -> 
+            match lastChar with
+            |'.' -> Some ("Hello") //sentence must end with a fullstop
+            |_ -> None
+          |_ -> None
+        |true -> None
   
 
 
@@ -116,5 +162,5 @@ let rivers input =
 let main argv =
     printfn "Hello World from F#!"
     
-    commaSprinkler "please sit spot. sit spot, sit. spot here now here." |> printfn "%A"
+    commaSprinkler "please sit spot. sit spot, sit. spot here now here." //|> printfn "%A"
     0 // return an integer exit code 
